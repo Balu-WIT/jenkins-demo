34ec188643a057bcc700d4f384cb361c033b9d8f
Balakrishna Mallela <balakrishna.mallela@wissen.com>
2024-06-06 20:39:56 +0530

    First commit

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator
+{
+	public int add(int a, int b) 
+	{
+		return a + b;
+	}
+	
+	public int sub(int a, int b) 
+	{
+		return a - b;
+	}
+	
+	/* public int mul(int a, int b) 
+	{
+		return a * b;
+	}
+	
+	public int div(int a, int b) throws ArithmeticException
+	{
+		if(b != 0)
+			return a / b;
+		else 
+			throw new ArithmeticException();
+	}
+	
+	public int mod(int a, int b) 
+	{
+		return a % b;
+	} */
+}
---
+++ CalculatorMain.java
@@ -0,0 +1,8 @@
+public class CalculatorMain
+{
+	public static void main(String args[])
+	{
+		Calculator c1 = new Calculator();
+		System.out.println("Addition of 20 and 10 is "+c1.add(20,10));
+	}
+}
